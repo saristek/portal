b849e219be00c0e0afd15ef07ead5e7fdf558f6d
sandikodev <androxoss@hotmail.com>
2023-09-25 05:14:42 +0700

    fix navbar spread grid

--- src/lib/components/Navbar/Nav.svelte
+++ src/lib/components/Navbar/Nav.svelte
@@ -7,7 +7,7 @@
 <div
 	class="sticky bottom-0 w-full bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600"
 >
-	<div class="grid h-full max-w-lg grid-cols-5 mx-auto">
+	<div class="grid grid-cols-5 mx-auto">
 		{#each NavMenu as menu}
 			<Button to={menu.to} icon={menu.icon}>{menu.title}</Button>
 		{/each}
