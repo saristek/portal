4e557dd8baaa6d9118c66d0f96140ef04d2f9294
Sandiko <androxoss@hotmail.com>
2025-01-06 21:07:35 +0700

    injectSpeedInsights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import '../app.css';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectSpeedInsights();
 	let { children } = $props();
 </script>
 
